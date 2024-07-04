f2efa18bc7ef81fd0ac45a8c3adf646992647526
siddheshdhavale <dhavale.s@northeastern.edu>
2024-07-04 11:30:05 -0400

    Route Navigate option added and successfully taking to Sign-up page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
+  // Opening a new Dynamic Block of code.
+  // This following code is if not authenticated react fragment which renders an outlet
   return (
-    <div>AuthLayout</div>
+    <>
+      { isAuthenticated ? (
+        <Navigate to ="/" />
+      ):
+      (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+
+
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +20,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+*/
