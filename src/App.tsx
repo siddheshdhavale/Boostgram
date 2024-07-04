08c353fdf088742979a0675a1778babc54f18710
siddheshdhavale <dhavale.s@northeastern.edu>
2024-07-04 10:29:35 -0400

    Router Browser set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Routes, Route} from 'react-router-dom';
+
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+//import Home from './_root/pages/Home';
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Boostgram!
-    </h1>
+    <main className="Flex h-screen">
+      <Routes>
+        {/* Public Routes*/}
+        <Route element={<AuthLayout />}> 
+          < Route path = "/sign-in" element={<SigninForm />}/>
+          < Route path = "/sign-up" element={<SignupForm />}/>
+        </Route>
+
+        {/* Private Routes*/}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />}/>
+          </Route>
+      </Routes>
+
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    // Now, the BrowserRouter controls the entire Routing for our website
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
