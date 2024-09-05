80c245d3b867a7bb8d3c37519f7cf500dcb2b0de
Siddhesh Dhavale <dhavale.s@northeastern.edu>
2024-09-05 11:18:12 -0400

    New updated files

--- vite.config.ts
+++ vite.config.ts
@@ -10,15 +10,3 @@ export default defineConfig({
     },
   },
 })
-
-
-/*
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-*/
